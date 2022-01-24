9d0b473dbd08fddf18dcf8bf7cdf998ce8cf49c2
annagolubkina <annatutta@gmail.com>
2022-01-24 13:23:50 +0300

    Final project ML in business
    Final project ML in business

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ML-_in_business_project-
+Итоговый проект по курсу "Машинное обучение в бизнесе"
+
+Стек: sklearn, numpy, pandas, xgboost, API: flask
+
+Данные: https://www.kaggle.com/teejmahal20/airline-passenger-satisfaction
+
+Задача:  Бинарная классификация: предсказать удовлетворенность пассажира сервисом авиакомпании по данным опроса.
+
+Преобразование признаков: QuantileTransformer, One Hot Encoding
+
+Для запуска:
+
+Запусть Flask-приложение: run_server.py
+Использовать функцию get_prediction из get_predictions.py для получения предсказанных значений
+
+Функция get_prediction: принимает на вход pandas-dataframe.Возвращает json-объект со списком предсказанных значений,следующего типа 'str': 'satisfied'/'neutral or dissatisfied'.
+
+Поддирректории:
+
+'datasets/' - содержит данные в формате .csv (train и test)
+'models/' - содержит предобученную модель в формате .dill
+'notebooks/' - содержит блокноты Jupyter Noutbook с этапами создания модели классификации
---
+++ run_server.py
@@ -0,0 +1,53 @@
+# USAGE
+# Start the server:
+# python run_server.py
+# Submit a request via cURL:
+# curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
+
+import dill
+import flask
+import pandas as pd
+import xgboost as xgb
+
+dill._dill._reverse_typemap['ClassType'] = type
+
+# Инициализация Flask приложения и модели
+app = flask.Flask(__name__)
+model = None
+
+
+def load_model(model_path):
+    # загрузка модели
+    global model
+    with open(model_path, 'rb') as f:
+        model = dill.load(f)
+
+
+@app.route("/", methods=["GET"])
+def general():
+    return "Welcome to airline passenger satisfaction prediction process"
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    # Инициализация словаря данных
+    data = {"success": False}
+
+    if flask.request.method == "POST":
+
+        request_json = flask.request.get_data(as_text=True)
+
+        df = pd.read_json(request_json)
+        preds = model.predict(df)
+        data["predictions"] = preds.tolist()
+        # indicate that the request was a success
+        data["success"] = True
+    # return the data dictionary as a JSON response
+    return flask.jsonify(data)
+
+if __name__ == "__main__":
+    print(("* Loading the model and Flask starting server..."
+           "please wait until server has fully started"))
+    modelpath = "../models/xgbclf_pipeline.dill"
+    load_model(modelpath)
+    app.run()
